32de7523f901ce2e91b2682b037cc477e38ad483
Filipe Cavalcanti <filipe-cavalcanti@outlook.com>
2022-07-29 17:29:40 -0300

    Começando projeto no Spring Boot / Banco de dados

---
+++ src/main/java/br/org/generation/blogPessoal/repository/PostagemRepository.java
@@ -0,0 +1,11 @@
+package br.org.generation.blogPessoal.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.org.generation.blogPessoal.model.Postagem;
+
+@Repository
+public interface PostagemRepository extends JpaRepository<Postagem, Long> {
+
+}
